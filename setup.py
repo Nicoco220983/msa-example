bc368c090272e31fee27b42b2c20a84d210b3b48
Nicolas Carrez <ncarrez@csod.com>
2021-10-21 20:43:04 +0200

    more standard naming convention

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='msa_example',
+    name='msa-example',
     version='0.1.0',    
     description='An example Python MSA package',
     author='Nicolas Carrez',
     author_email='nicolas.carrez@gmail.com',
     license='BSD 2-clause',
-    packages=['msa_example'],
-    package_data={'msa_example': ['static/*']},
+    packages=['msaexample'],
+    package_data={'msaexample': ['static/*']},
     include_package_data=True,
     install_requires=[
         'fastapi',
